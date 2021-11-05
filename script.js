3e68860f8048d8ed0d7f18bce0c70f1040fab7d0
rajibjonchhen <51174335+rajibjonchhen@users.noreply.github.com>
2021-11-05 01:18:06 +0000

    random number selected on the board

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 
 
+
 const getBoard = function(){
     return document.getElementById("cells-container")
 }
 
+const getTicket = function(){
+    return document.getElementById("tickets-container")
+}
 
 const displayNumbers = function (){
     let boardNode = getBoard()
@@ -16,19 +20,69 @@ const displayNumbers = function (){
 }
 displayNumbers()
 
-const selectRandomNumber = function(event){
-    
-
+let numbersOnTicket =[]
+const displayTicket = function (){
+    let tickets = getTicket()
+    for(let i = 1; i <= 24; i++){
+        let ticketNode = document.createElement("div")
+        ticketNode.innerText = (Math.floor(Math.random()*76))
+        numbersOnTicket[i] =  ticketNode.innerText
+        ticketNode.classList.add("cellOnTicket")
+        tickets.appendChild(ticketNode)
+       
+    }
 }
+displayTicket()
+
+const play = function(){
+    let getNumber = document.getElementsByClassName("cell")
+    let selectedNumber = Math.floor(Math.random()*76)
+    for(let i = 1; i <= 76; i++){
+        if(getNumber[i].innerText == selectedNumber){ 
+        getNumber[i].style.backgroundColor = "red"
+        
+        }
+
+    }   
+}
+play()
 
 
-const getTickets = function(){
-    ticketlist = document.getElementById("ticket-container")
+/*
+
+const getTickets = function(n){
     let ticket = []
+    let ticketlist = []
+     document.getElementById("ticket-container")
     for(let i = 1 ; i <=24; i++){
-       let number = Math.floor(math.selectRandomNumber()*76)
-    ticket.push(number)
-    ticketList += appendChild(ticket)
+        let number = selectRandomNumber ()
+        ticket.push(number)
+        ticketlist += ticket
     }
 }
 getTickets()
+
+
+const selectRandomNumber = function(){
+    let number = Math.floor(math.selectRandomNumber()*76)
+     return number
+}
+
+
+const createTicket = function(){
+    let ticket = []
+    for(let i = 1; i <= 24; i++){
+       addEventListener =("onclick",  ticket.push(selectRandomNumber)  
+    }
+    return ticket
+}
+createTicket()
+
+
+/*
+
+window.onload = function(){
+    
+}
+
+*/
